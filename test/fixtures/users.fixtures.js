2bd219c70953172dd2a5cdbc8cc2de7e16e3c366
JG-Jeong <jo128g@naver.com>
2022-10-20 03:55:14 +0900

    test code repository작성 완료

--- test/fixtures/users.fixtures.js
+++ test/fixtures/users.fixtures.js
@@ -3,12 +3,18 @@
 // users repo 에서 findUserAccount 찾을때 쓰는 스키마
 exports.findUserInsertSchema ={
     email: 'asdlkf124j@naver.com',
-    nickname: 'nickname',
+    nickname: '스파르타1213',
 };
 
 // users repo 에서 createAccount 찾을때 쓰는 스키마
 exports.createUserInsertSchemaByRepository = {
-    email: 'archepro85',
+    email: 'asdlkf124j@naver.com',
     nickname: '스파르타11',
     password: '1234'
 };
+
+//로그인 할때 가져가는 스키마
+exports.loginUserSchemaByRepository = {
+    email: 'asdlkf124j@naver.com',
+    password: '1234'
+}
