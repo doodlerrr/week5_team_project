bc4a7c317a92f5926f071ccbb6f348deece70cbc
오윤지 <network_dude@naver.com>
2022-10-20 13:25:29 +0900

    포스트 에러처리

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const usersRouter = require('./users.routes'); // 회원가입, 로그인
 const postsRouter = require('./posts.routes'); // 포스트,좋아요
 const commentsRouter = require('./comments.routes'); // 댓글
 
-router.use('/users', usersRouter); // 회원가입, 로그인
-router.use('/posts', postsRouter); // 포스트, 좋아요
-router.use('/comments', commentsRouter) // 뎃글
+router.use('/users', usersRouter);      // 회원가입, 로그인
+router.use('/posts', postsRouter);      // 포스트, 좋아요
+router.use('/comments', commentsRouter) // 댓글
 
 module.exports = router;
